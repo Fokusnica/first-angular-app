98084e752da6cff0d39721bf3babee9d14118e6c
Derevianko Liubov <dereviankoliubov@gmail.com>
2015-09-21 23:30:19 +0300

    add references

--- app.js
+++ app.js
@@ -12,16 +12,28 @@ var bikes = [
 	price: 1840,
 	description: "The Bad Boy's stealth-fighter looks and street-fighter performance make it a fast, stylish and functional partner-in-crime for all your urban adventures.",
     canPurchase: true,
-    soldOut: false
+    soldOut: false,
+    images: [
+    {
+    	full: 'bad_boy1.jpg',
+    	thumb: 'bad_boy2.jpg'
+    }
+    ]
 
 },
 {
     name: "JEKYLL 27.5",
 	type: "Mountain",
 	price: 7990,
-	description: "Jekyll’s two-bikes-in-one split personalities let you love the climbs and KILL the descents.",
+	description: "Jekyll's two-bikes-in-one split personalities let you love the climbs and KILL the descents.",
     canPurchase: true,
-    soldOut: false
+    soldOut: false,
+    images: [
+    {
+    	full: 'jekill1.jpg',
+    	thumb: 'jekill2.jpg'
+    }
+    ]
 
 }
 ];
